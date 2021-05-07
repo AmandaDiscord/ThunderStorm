1dcaa43d42f7252f9f1e2a85b72978fc3475952b
Papi <bradleyjplatt@yahoo.com>
2021-05-06 19:32:01 -0700

    ignore dist and delete. New src branch

--- dist/structures/Partial/PartialUser.d.ts
+++
@@ -1,8 +0,0 @@
-import PartialBase from "./PartialBase";
-declare class PartialUser extends PartialBase<import("../User")> {
-    partialType: "User";
-    constructor(data: import("../../internal").PartialData, client: import("../Client"));
-    toString(): string;
-    send(content: import("../../Types").StringResolvable, options?: import("../../Types").MessageOptions): Promise<import("../Message")>;
-}
-export = PartialUser;
