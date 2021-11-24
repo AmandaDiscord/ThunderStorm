8c5f3a5e350cda28b3e61fb32c3abcb3d340dcab
Papa <bradleyjplatt@yahoo.com>
2021-11-24 00:04:10 -0700

    Remove dep on @sapphire/snowflake

--- ts/util/SnowflakeUtil.ts
+++ ts/util/SnowflakeUtil.ts
@@ -1,13 +1,11 @@
 // THIS FILE HAS BEEN MODIFIED FROM DISCORD.JS CODE
-import { DiscordSnowflake } from "@sapphire/snowflake";
-
 import Util from "./Util";
 
-const EPOCH = Number(DiscordSnowflake.Epoch);
+// Discord epoch (2015-01-01T00:00:00.000Z)
+const EPOCH = 1_420_070_400_000;
+let INCREMENT = 0;
 
 class SnowflakeUtil {
-	public static EPOCH = EPOCH;
-
 	public static readonly default = SnowflakeUtil;
 
 	public constructor() {
@@ -15,20 +13,36 @@ class SnowflakeUtil {
 	}
 
 	static generate(timestamp: number | Date = Date.now()) {
-		return DiscordSnowflake.generate({ timestamp: timestamp }).toString();
+		if (timestamp instanceof Date) timestamp = timestamp.getTime();
+		if (typeof timestamp !== "number" || isNaN(timestamp)) {
+			throw new TypeError(
+				`"timestamp" argument must be a number (received ${isNaN(timestamp) ? "NaN" : typeof timestamp})`
+			);
+		}
+		if (INCREMENT >= 4095) INCREMENT = 0;
+		const BINARY = `${(timestamp - EPOCH).toString(2).padStart(42, "0")}0000100000${(INCREMENT++)
+			.toString(2)
+			.padStart(12, "0")}`;
+		return Util.binaryToId(BINARY);
 	}
 
 	static deconstruct(snowflake: string) {
-		const deconstructed = DiscordSnowflake.deconstruct(snowflake);
-		const timestamp = Number(deconstructed.timestamp);
+		// @ts-ignore
+		const BINARY = Util.idToBinary(snowflake).toString(2).padStart(64, "0");
 		return {
-			timestamp: timestamp,
-			date: new Date(timestamp),
-			workerId: Number(deconstructed.workerID),
-			processId: Number(deconstructed.processID),
-			increment: Number(deconstructed.increment),
-			binary: Util.idToBinary(snowflake)
+			timestamp: parseInt(BINARY.substring(0, 42), 2) + EPOCH,
+			get date() {
+				return new Date(this.timestamp);
+			},
+			workerId: parseInt(BINARY.substring(42, 47), 2),
+			processId: parseInt(BINARY.substring(47, 52), 2),
+			increment: parseInt(BINARY.substring(52, 64), 2),
+			binary: BINARY
 		} as import("../Types").DeconstructedSnowflake;
+	}
+
+	public static get EPOCH() {
+		return EPOCH;
 	}
 }
 
