af51b0ebd84cfb476aa42521e13720d0fc2c56ee
Papa <bradleyjplatt@yahoo.com>
2022-02-14 20:51:03 -0700

    Fix SelectMenuInteraction mapping undefined

--- src/structures/SelectMenuInteraction.js
+++ src/structures/SelectMenuInteraction.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 const MessageComponentInteraction_1 = __importDefault(require("./MessageComponentInteraction"));
 class SelectMenuInteraction extends MessageComponentInteraction_1.default {
     constructor(client, data) {
-        var _a, _b, _c;
+        var _a, _b;
         super(client, data);
-        this.values = (_c = (_b = (_a = data.data) === null || _a === void 0 ? void 0 : _a.values) === null || _b === void 0 ? void 0 : _b.map(i => i.value)) !== null && _c !== void 0 ? _c : [];
+        this.values = (_b = (_a = data.data) === null || _a === void 0 ? void 0 : _a.values) !== null && _b !== void 0 ? _b : [];
     }
 }
 SelectMenuInteraction.default = SelectMenuInteraction;
